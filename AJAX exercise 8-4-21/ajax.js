ec25d346dd32577977c557bf34aa04c8072fc5ad
unknown <Lntinfotech@INFEDL31167.lntinfotech.com>
2021-08-04 17:01:28 -0400

    added the finished AJAX exercise for 8-4-21

---
+++ AJAX exercise 8-4-21/ajax.js
@@ -0,0 +1,36 @@
+// document.getElementById('getData').onclick = getData;
+document.getElementById('getData').addEventListener("click", getData);
+
+let apiURL = 'https://pokeapi.co/api/v2/pokemon/';
+
+function getData() {
+
+    let userInput = document.getElementById('dataInput').value;
+
+    let xhr = new XMLHttpRequest();
+
+    xhr.onreadystatechange = receiveData;
+
+    xhr.open('GET', apiURL + userInput);
+
+    xhr.send();
+
+    function receiveData(){
+        if(xhr.readyState === 4 && xhr.status >= 200 && xhr.status < 300){
+            let response = xhr.responseText;
+            response = JSON.parse(response);
+            populateData(response);
+        }
+    }
+  
+}
+
+function populateData(response) {
+    console.log(response);
+    // Display pokemon information in the section tag of my html document
+    let data =  document.getElementById('data');
+    let info = document.createTextNode(response.name);
+    
+    data.appendChild(info);
+
+}
