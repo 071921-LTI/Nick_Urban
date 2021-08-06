cbda54ea8b0800c103ac5ed343095b5edcc8da23
unknown <Lntinfotech@INFEDL31167.lntinfotech.com>
2021-08-06 17:13:33 -0400

    finished adding the constructors, getters/setters equals/hashcode, and tostring methods to all model classes.

--- Project1/src/main/java/models/ReimbursementStatus.java
+++ Project1/src/main/java/models/ReimbursementStatus.java
@@ -3,4 +3,77 @@ package models;
 public class ReimbursementStatus {
 	private int id;
 	private String status;
+	
+	
+	// Constructors
+	public ReimbursementStatus() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public ReimbursementStatus(String status) {
+		super();
+		this.status = status;
+	}
+
+	public ReimbursementStatus(int id, String status) {
+		super();
+		this.id = id;
+		this.status = status;
+	}
+
+	
+	// Getters and setters
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	
+	// Equals and Hashcode
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + id;
+		result = prime * result + ((status == null) ? 0 : status.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		ReimbursementStatus other = (ReimbursementStatus) obj;
+		if (id != other.id)
+			return false;
+		if (status == null) {
+			if (other.status != null)
+				return false;
+		} else if (!status.equals(other.status))
+			return false;
+		return true;
+	}
+
+	
+	// To string
+	@Override
+	public String toString() {
+		return "ReimbursementStatus [id=" + id + ", status=" + status + "]";
+	}
 }
--- Project1/src/main/java/models/ReimbursementType.java
+++ Project1/src/main/java/models/ReimbursementType.java
@@ -3,4 +3,77 @@ package models;
 public class ReimbursementType {
 	private int id;
 	private String type;
+	
+	
+	// Constructors
+	public ReimbursementType() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public ReimbursementType(String type) {
+		super();
+		this.type = type;
+	}
+
+	public ReimbursementType(int id, String type) {
+		super();
+		this.id = id;
+		this.type = type;
+	}
+
+	
+	// Getters and setters
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	
+	// Equals and Hashcode
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + id;
+		result = prime * result + ((type == null) ? 0 : type.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		ReimbursementType other = (ReimbursementType) obj;
+		if (id != other.id)
+			return false;
+		if (type == null) {
+			if (other.type != null)
+				return false;
+		} else if (!type.equals(other.type))
+			return false;
+		return true;
+	}
+
+	
+	// To string
+	@Override
+	public String toString() {
+		return "ReimbursementType [id=" + id + ", type=" + type + "]";
+	}	
 }
